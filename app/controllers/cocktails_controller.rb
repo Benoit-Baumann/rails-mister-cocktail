5ffba168fe7c29be568759c17b1c27a556c39921
Benoit Baumann <baumann.benoit@gmail.com>
2019-11-07 15:49:14 +0100

    back-end functionality implemented

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,38 @@
+class CocktailsController < ApplicationController
+    before_action :set_cocktail, only: [:show]
+
+    # GET /cocktails
+    def index
+        @cocktails = Cocktail.all
+    end
+
+    # GET /cocktails/:id
+    def show
+    end
+
+    # Get /cocktails/new
+    def new
+        @cocktail = Cocktail.new
+    end
+
+    # POST /cocktails
+    def create
+        @cocktail = Cocktail.new(cocktail_params)
+
+        if @cocktail.save
+            redirect_to cocktails_path, notice: 'Restaurant was successfully created.'
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def cocktail_params
+        params.require(:cocktail).permit(:name)
+    end
+
+    def set_cocktail
+        @cocktail = Cocktail.find(params[:id])
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 require 'open-uri'
 require 'json'
 
+puts "Deleting all the ingredients..."
+Ingredient.destroy_all
+
+puts "Populating the database with new ingredients..."
 response = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 json = JSON.parse(response)
 json['drinks'].each do |element|
